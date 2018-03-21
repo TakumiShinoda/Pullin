7572f2aaaee5f1a46261aaa597cf370b00851edf
TakumiShinoda <takumishinoda@TakumiShinodanoMacBook-Pro.local>
2018-03-22 00:46:43 +0900

    add contents selector

--- src/assets/javascript/common.js
+++ src/assets/javascript/common.js
@@ -1,2 +1,14 @@
 $(document).ready(() => {
+  $('.menuSelector').on('click', (ev) => {
+    hide('.contents');
+    show('.' + ev.target.attributes.name.value);
+  });
 });
+
+function hide(ele){
+  $(ele).css('display', 'none');
+}
+
+function show(ele){
+  $(ele).css('display', 'block');
+}
