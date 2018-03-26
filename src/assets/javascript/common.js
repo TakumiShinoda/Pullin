8072b98c4064959b9d406c4bc5b7ae3f61f4043d
TakumiShinoda <godgdox4@gmail.com>
2018-03-27 04:22:11 +0900

    add connection renderer and webview

--- src/assets/javascript/common.js
+++ src/assets/javascript/common.js
@@ -3,6 +3,17 @@ $(document).ready(() => {
     hide('.contents');
     show('.' + ev.target.attributes.name.value);
   });
+
+  var webview = document.getElementById('searchView');
+  console.log($('#searchView'));
+
+  webview.openDevTools();
+  webview.addEventListener('did-finish-load', () => {
+    console.log('fin')
+    webview.executeJavaScript('console.log($(":hover"));', false, () => {
+      console.log("suc");
+    })
+  });
 });
 
 function hide(ele){
