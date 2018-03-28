aba57f217bf05cd65035cb7357164144d9158d83
TakumiShinoda <takumishinoda@TakumiShinodanoMacBook-Pro.local>
2018-03-28 22:46:50 +0900

    add element selector

--- src/assets/javascript/index/webviewResources/afterLoad.js
+++ src/assets/javascript/index/webviewResources/afterLoad.js
@@ -1,2 +1,18 @@
 console.log('load finished');
-console.log($(':hover'));
+
+var tags = document.getElementsByTagName("*");
+
+function elementSelect(){
+  var ele = $(':hover');
+
+  ele[ele.length - 1].style.backgroundColor = 'rgba(122, 0, 0, 0.5)';
+}
+
+function elementOut(ele){
+  ele.style.backgroundColor = '';
+}
+
+for(var i = 0; i < tags.length; i++){
+  tags[i].setAttribute('onmouseover', 'elementSelect()')
+  tags[i].setAttribute('onmouseout', 'elementOut(this)')
+}
