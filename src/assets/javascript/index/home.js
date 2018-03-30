ae5f34ef089a6c19d9103e1a7bc6b0d953bdce0b
TakumiShinoda <takumishinoda@TakumiShinodanoMacBook-Pro.local>
2018-03-30 20:06:45 +0900

    change some name of sources

--- src/assets/javascript/index/home.js
+++ src/assets/javascript/index/home.js
@@ -7,14 +7,14 @@ $(document).ready(() => {
       // n.openDevTools();
       n.src = 'http://www.gooogle.com';
       n.addEventListener('dom-ready', () => {
-        $.get('../../js/index/webviewResources/newsWebView/newsBeforeLoad.js', (text) => {
+        $.get('../../js/index/webviewResources/newsWebView/beforeLoad.js', (text) => {
           n.executeJavaScript(text, false, () => {
             console.log("before load");
           });
         });
       });
       n.addEventListener('did-finish-load', () => {
-        $.get('../../js/index/webviewResources/newsWebView/newsAfterLoad.js', (text) => {
+        $.get('../../js/index/webviewResources/newsWebView/afterLoad.js', (text) => {
           n.executeJavaScript(text, false, () => {
             console.log("load finished");
           });
