15ebb9a8c4b561db10747c92642d8ff8d16cc91f
TakumiShinoda <takumishinoda@TakumiShinodanoMacBook-Pro.local>
2018-04-19 13:54:10 +0900

    apply for flash at webview

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
 const electron = require('electron');
+const path = require('path');
+
+let pluginName
+
+switch(process.platform){
+  case 'win32':
+    pluginName = 'pepflashplayer.dll'
+    break
+  case 'darwin':
+    pluginName = 'PepperFlashPlayer.plugin'
+    break
+  case 'linux':
+    pluginName = 'libpepflashplayer.so'
+    break
+}
+electron.app.commandLine.appendSwitch('ppapi-flash-path', path.join(__dirname, pluginName))
+
+electron.app.commandLine.appendSwitch('ppapi-flash-version', '17.0.0.169')
 
 electron.app.on('ready', () => {
   mainWindow = new electron.BrowserWindow({
@@ -6,6 +24,9 @@ electron.app.on('ready', () => {
     height: 800,
     resizable: false,
     movable: true,
+    webPreferences: {
+      plugins: true
+    },
   });
   mainWindow.loadURL('file://' + __dirname + '/dist/views/index/index.html');
 
