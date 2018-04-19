502c1100e462d364020e34898f70b83038ae5b0d
TakumiShinoda <takumishinoda@TakumiShinodanoMacBook-Pro.local>
2018-04-19 13:58:22 +0900

    clean up main process code

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const electron = require('electron');
+const {app, BrowserWindow} = require('electron');
 const path = require('path');
 
 let pluginName
@@ -14,12 +14,11 @@ switch(process.platform){
     pluginName = 'libpepflashplayer.so'
     break
 }
-electron.app.commandLine.appendSwitch('ppapi-flash-path', path.join(__dirname, pluginName))
+app.commandLine.appendSwitch('ppapi-flash-path', path.join(__dirname, pluginName))
+app.commandLine.appendSwitch('ppapi-flash-version', '17.0.0.169')
 
-electron.app.commandLine.appendSwitch('ppapi-flash-version', '17.0.0.169')
-
-electron.app.on('ready', () => {
-  mainWindow = new electron.BrowserWindow({
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
     width: 1280,
     height: 800,
     resizable: false,
